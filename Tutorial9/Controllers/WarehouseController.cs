222e6e20b884d22bedc505546cf1f7f0b8beaba4
IvanMazhara <s31385@pjwstk.edu.pl>
2025-05-11 12:26:55 +0200

    Controller operations such as object check/ GET request by id implemented

--- Tutorial9/Controllers/WarehouseController.cs
+++ Tutorial9/Controllers/WarehouseController.cs
@@ -1,10 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Tutorial9.Services;
 
 namespace Tutorial9.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class WarehouseController
+public class WarehouseController : ControllerBase
 {
+    private readonly IWarehouseService _warehouseService;
+    public WarehouseController(IWarehouseService warehouseService)
+    {
+        _warehouseService = warehouseService;
+    }
     
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetWarehouse(int id)
+    {
+        var warehouse = await _warehouseService.GetWarehouse(id);
+        if (warehouse is null)
+            return NotFound();
+
+        return Ok(warehouse);
+    }
+
+    [HttpGet("{id}/exists")]
+    public async Task<IActionResult> CheckWarehouse(int id)
+    {
+        var exists = await _warehouseService.WithGivenIdExists(id);
+        return Ok(exists);
+    }
 }
