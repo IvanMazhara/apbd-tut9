8340d885d25c55a905d9d0b8127e4d13e7f8f7b6
IvanMazhara <s31385@pjwstk.edu.pl>
2025-05-11 10:25:55 +0200

    Check based on Id implemented.

---
+++ Tutorial9/Services/WarehouseService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Tutorial9.Services;
+
+public class WarehouseService : IWarehouseService
+{
+    private readonly IConfiguration _configuration;
+
+    public WarehouseService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task<bool> WithGivenIdExists(int Id)
+    {
+        var query = "SELECT 1 FROM Warehouse WHERE Id = @ID";
+        
+        await using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        await using SqlCommand command = new SqlCommand();
+
+        command.Connection = connection;
+        command.CommandText = query;
+        command.Parameters.AddWithValue("@ID", Id);
+
+        await connection.OpenAsync();
+        
+        var res = await command.ExecuteScalarAsync();
+
+        return res is not null;
+    }
+}
