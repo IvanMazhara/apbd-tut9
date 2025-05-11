312a1f0430096a069eee4f7a528b361959f50498
IvanMazhara <s31385@pjwstk.edu.pl>
2025-05-11 10:34:44 +0200

    WarehouseController.cs

--- Tutorial9/Controllers/WarehouseController.cs
+++ Tutorial9/Controllers/WarehouseController.cs
@@ -1,5 +1,9 @@
-﻿namespace Tutorial9.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
 
+namespace Tutorial9.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
 public class WarehouseController
 {
     
